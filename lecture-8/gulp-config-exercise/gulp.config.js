8382d4e991cbaff269d3747c5ff47f72e4182fa7
Niels Henrik Juul Hansen <niha@eaaa.dk>
2016-04-06 09:49:53 +0200

    changed config

--- lecture-8/gulp-config-exercise/gulp.config.js
+++ lecture-8/gulp-config-exercise/gulp.config.js
@@ -2,11 +2,7 @@ function gulpConfig(){
     "use strict";
 	
 	var config = {
-        baseDir: "./",
-		browserSyncFiles: ["./*.html", "./css/*.css"],
-        browsers: ["firefox", "google chrome"],
-        sass: "sass/*.scss",
-        css: "./css"
+        
 	};
 
 	return config;
